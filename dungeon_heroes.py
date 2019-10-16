26b73e07d8bef725a1b3bdf633ca8d92c1561a79
Christopher Barnes <cbarnes7404@yahoo.com>
2019-10-16 14:37:41 -0700

    adjusted intro and start of game mechanics

--- dungeon_heroes.py
+++ dungeon_heroes.py
@@ -1,18 +1,43 @@
 from hero import Hero
 
 if __name__ == "__main__":
+    #Intro
+    print("""
+    ******----******----******
+    Welcome to Dungeon Heroes!
+    ******----******----******
+    
+    \^~~~~\\   )  (   /~~~~^/
+     ) *** \\  {**}  / *** (
+      ) *** \\_ ^^ _/ *** (
+      ) ****   vv   **** (
+       )_****      ****_(
+        )*** m  m ***(
+    """)
+    
+    print("Select (N)ew game or (C)ontinue")
+    response = getResponse()
+    if(response.equals("C")):
+        pass
+        #try:
+            #g_player = SaveGame.load("saveGame.txt")
+            #print("File Loaded")
+        #catch(FileNotFoundException e):
+            #print("SaveGame not found")
+            #createCharacter(g_input)
+    else:
+        print("********")
+        print("NEW GAME")
+        print("********")
+        #Create a character
+        hero = Hero("SandBoi", "Warrior")
+        #createCharacter(g_input) # player chooses their player class
+        #g_player.status() # tests player and shows us status
+
+
+    #Main loop to play game
+    print()
     game_is_running = True
-
-    #Create a character
-    hero = Hero("SandBoi", "Warrior")
-
-    #Create teams
-
-    #Create a new area
-
-
-"""
-    #play game
     while(game_is_running):
         print("Starting battle")
         get_in = input("Play Again? Y or N: \t")
@@ -23,4 +48,12 @@ if __name__ == "__main__":
         else:
             #resart to play again
             pass
+"""
+boolean playGame = true
+    #starting room
+    if(startRoom(g_input).equals("L")):
+        playGame = false
+    while(playGame):
+        playGame = enterRoom(g_input); # Keep playing until the user quits
+
 """
